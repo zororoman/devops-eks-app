9ac9a7110f1a2153fddc1c9dce08c848ed223c40
Ajaykumar ramesh <133523268+zororoman@users.noreply.github.com>
2026-02-02 03:02:30 +0530

    Add health check endpoint and serve static files

--- app.js
+++ app.js
@@ -1,10 +1,13 @@
 const express = require("express");
 const app = express();
 
-app.get("/", (req, res) => {
-  res.send("DevSecOps pipeline running 🚀");
+app.use(express.static("public"));
+
+app.get("/health", (req, res) => {
+  res.json({ status: "UP" });
 });
 
+const PORT = 3000;
 app.listen(3000, () => {
   console.log("App running on port 3000");
 });
